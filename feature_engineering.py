911e3cbca8f6c39a0aa31a2cf727d0173d87140f
Lauramdv <lauramariannedv@gmail.com>
2025-06-11 10:35:26 +0200

    Temporal Abstraction

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from TemporalAbstraction import CategoricalAbstraction 
+
 
 df = pd.read_csv('Glucose_export.csv')
 df['Timestamp'] = pd.to_datetime(df['Timestamp'])
@@ -44,11 +46,58 @@ print(df.columns)
 print(df.head(20))
 
 
-#remove redundant columns
+#remove redundant columns, to be expanded
 redundant = ["glucose_diff"]
 df.drop(columns=redundant)
 
 
-# df.to_csv("feature.csv")
+#CategoricalAbstraction
+# Optionally fill NaNs with a placeholder to avoid issues during encoding
+df['glucose_trend'] = df['glucose_trend'].fillna('unknown')
+df['cat_glucose_value (mmol/l)'] = df['cat_glucose_value (mmol/l)'].fillna('unknown')
+df['Insulinetype'] = df['Insulinetype'].fillna('unknown')
+df['Insulinetype_bolus'] = df['Insulinetype_bolus'].fillna('unknown')
+df['Alarm'] = df['Alarm'].fillna('unknown')
+
+# One-hot encode selected categorical columns
+categorical_columns = [
+    'glucose_trend', 
+    'cat_glucose_value (mmol/l)', 
+    'Insulinetype', 
+    'Insulinetype_bolus', 
+    'Alarm'
+]
+
+df_encoded = pd.get_dummies(df, columns=categorical_columns)
+df_encoded.to_csv("features_onehot.csv", index=False)
+
+cat_abs = CategoricalAbstraction()
+
+#Get binary columns used in pattern mining
+cols = [col for col in df_encoded.columns if col.startswith((
+    'glucose_trend_', 
+    'cat_glucose_value (mmol/l)_', 
+    'Insulinetype_', 
+    'Insulinetype_bolus_', 
+    'Alarm_'
+))] + ['Only basal'] 
+
+match = ['like'] * len(cols)
+min_support = 0.2
+window_size = 6
+max_pattern_size = 3
+
+abstracted_df = cat_abs.abstract_categorical(
+    data_table=df_encoded.copy(),
+    cols=cols,
+    match=match,
+    min_support=min_support,
+    window_size=window_size,
+    max_pattern_size=max_pattern_size
+)
+
+abstracted_df.to_csv("featiures_with_patterns.csv", index=False)
+
+df.to_csv("feature.csv")
 
 
