7d20d93563ac9e15bf5bd1e79965d6724cb6f1b1
Lauramdv <lauramariannedv@gmail.com>
2025-06-13 10:21:51 +0200

    small changes

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,15 +6,7 @@ def feature_engineering(df, min_support, window_size, max_pattern_size):
     df['Timestamp'] = pd.to_datetime(df['Timestamp'])
     df.set_index('Timestamp', inplace=True)
 
-    #Set the rolling window to 30 minutes checken
-    window = '30min'
-    numeric_columns = df.select_dtypes(include='number').columns
-    new_features = ['min', 'max', 'mean', "median",  'std']
-
-    for col in numeric_columns:
-        for feature in new_features:
-            agg_glucose = df[col].rolling(window).agg([feature])
-            df[feature + '_' + col] = agg_glucose[feature]
+    df = df.drop(columns = ['Duration (minutes)'])
 
     #categorize GCM values
     conditions = [
@@ -40,6 +32,17 @@ def feature_engineering(df, min_support, window_size, max_pattern_size):
     #Create new column with total insuling and if it only consists of basal --> more interesting to do 1 IF both present?
     df['Insulin_units_total'] = df['Insuline units (basal)'] + df['Insuline units (bolus)']
     df['Only basal'] = ((df['Insuline units (bolus)'].isna()) | (df['Insuline units (bolus)'] == 0)).astype(int)
+
+    #Set the rolling window to 30 minutes checken
+    window = '30min'
+    numeric_columns = ["Glucose value (mmol/l)", "Insuline units (basal)" ,"Insulin_units_total", 
+                       "BG_input (mmol/l)", "Carbohydrates (g)",	"Carb ratio","Insuline units (bolus)"]
+    new_features = ['min', 'max', 'mean', "median",  'std']
+
+    for col in numeric_columns:
+        for feature in new_features:
+            agg_glucose = df[col].rolling(window).agg([feature])
+            df[feature + '_' + col] = agg_glucose[feature]
 
     #Day of week feature
     df['day_of_week'] = df.index.dayofweek
@@ -85,8 +88,20 @@ def feature_engineering(df, min_support, window_size, max_pattern_size):
         max_pattern_size=max_pattern_size
     )
 
-    return abstracted_df
+    #create target
+    future_window = 6
 
+    abstracted_df['target_majority_category'] = (
+        abstracted_df['cat_glucose_value (mmol/l)']
+        .shift(-future_window + 1)  # shift upward to align current row with future window
+        .rolling(window=future_window)
+        .apply(lambda x: x.mode().iloc[0] if len(x) > 0 else np.nan)
+    )
+
+    # Drop last rows where the label cannot be calculated
+    abstracted_df = abstracted_df.dropna(subset=['target_majority_category'])
+
+    return abstracted_df
 
 
 df_train = pd.read_csv("Glucose_export_imputed.csv")
@@ -95,10 +110,7 @@ df_test = pd.read_csv("Glucose_export_imputed_test.csv")
 # Add a column to distinguish between train and test
 df_train['__set__'] = 'train'
 df_test['__set__'] = 'test'
-
-# Append the two datasets
 df_combined = pd.concat([df_train, df_test], ignore_index=True)
-
 
 df_combined_features = feature_engineering(df_combined, 0.2,3,3)
 df_train_features = df_combined_features[df_combined_features['__set__'] == 'train'].drop(columns='__set__')
